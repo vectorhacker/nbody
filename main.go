ec300394f7c54684fab0faa2e1025a1d77665d77
Victor A. Martinez Santiago <victor@plazsa.com>
2017-09-13 05:57:41 -0400

    added use of logrus, new build, run, clean commands, and handled out of range case for Atoi

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math"
 	"os"
 	"strconv"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/vectorhacker/nbody/nbody"
 )
 
 func main() {
 	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	switch e := err.(type) {
+	case *strconv.NumError:
+		log.Warnf("%s, using maximum 32 bit integer value of %d", e, math.MaxInt32)
+		n = math.MaxInt32
+	default:
+		if err != nil {
+			log.Fatal(e)
+		}
 	}
 
-	bodies := nbody.NewSystem()
+	bodies := nbody.Default()
 	fmt.Printf("%.9f\n", bodies.Energy())
 	for i := 0; i < n; i++ {
 		bodies.Advance(0.01)
